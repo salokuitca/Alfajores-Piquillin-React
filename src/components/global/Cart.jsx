ffbfdd5bb4a7b85a7bd4a9c9d1c8c7850dff795a
Salome Kuitca <salokuitca@gmail.com>
2021-01-13 19:01:34 -0300

    Renderizado condicional con CartVacio en otro componente

--- src/components/global/Cart.jsx
+++ src/components/global/Cart.jsx
@@ -1,10 +1,9 @@
 import {useContext} from 'react';
 import "../../styles/cart.css";
-// import {CartVacio} from './CartVacio';
+import CartVacio from './CartVacio';
 import {Store} from '../../store';
 import {useHistory} from 'react-router-dom';
-import {Link} from 'react-router-dom';
-import cartEmpty from "../../assets/cartEmpty.png";
+
 
 const Cart = () => {
     const [data, setData] = useContext(Store);
@@ -113,16 +112,8 @@ const Cart = () => {
 
         : 
         <>
-        <div className="container carrito-vacio">
-        <div className="d-flex justify-content-center align-items-center"> Oh no! Tu carrito está vacío! </div>
-        <div className="d-flex justify-content-center align-items-center">
-        <img src={cartEmpty} alt="carrito vacio"/>
-        </div>
-             <Link to={"/"} className="text-decoration-none d-flex justify-content-center align-items-center">
-                <button className="btn btn-lg btn-block btn-warning shadow-sm rounded-pill m-2 col-12 col-md-6 "> Quiero comprar!!!</button>
-            </Link>
-        </div>  
-        {/* <CartVacio></CartVacio> */}
+        
+         <CartVacio/>
         </>
         }
     </div>
--- src/components/global/CartVacio.jsx
+++ src/components/global/CartVacio.jsx
@@ -1,17 +1,19 @@
 import '../../styles/cart.css';
+import {Link} from 'react-router-dom';
+import cartEmpty from "../../assets/cartEmpty.png";
 
 const CartVacio = () => {
     return (
         <>
         <div className="container carrito-vacio">
-        <div className="d-flex justify-content-center align-items-center"> Oh no! :( Tu carrito está vacío! </div>
+        <div className="d-flex justify-content-center align-items-center"> Oh no! Tu carrito está vacío! </div>
         <div className="d-flex justify-content-center align-items-center">
-        <img src="images/cartEmpty.png" alt="carrito vacio"/>
+        <img src={cartEmpty} alt="carrito vacio"/>
         </div>
-        <a href="index.html#productos" className="text-decoration-none d-flex justify-content-center align-items-center">
-                    <button className="btn btn-lg btn-block btn-warning shadow-sm rounded-pill m-2 col-12 col-md-6 "> Quiero comprar!!!</button>
-                </a>
-    </div> 
+             <Link to={"/"} className="text-decoration-none d-flex justify-content-center align-items-center">
+                <button className="btn btn-lg btn-block btn-warning shadow-sm rounded-pill m-2 col-12 col-md-6 "> Quiero comprar!!!</button>
+            </Link>
+        </div>  
         </>
     )
 }
